953024608036e4cfe0ad6597fa103ab3f3896268
Natalia Kostka <natalia7kostka@gmail.com>
2022-11-21 14:42:48 -0700

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+
 const app = express();
 
 //middelware
@@ -12,4 +18,4 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Servered started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
