7a8aeb34646cf764f814c12328a46fb92ed08a60
Natalia Kostka <natalia7kostka@gmail.com>
2022-11-22 14:56:47 -0700

    finished frontend working on backend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require('express')
 const colors = require('colors')
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
 const port = process.env.PORT || 3000;
 
-connectDB()
+connectDB();
 
 const app = express();
 
-//middelware
+//middleware
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
